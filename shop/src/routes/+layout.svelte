71c4f9dc3a7b482c3f88dd3440a95999e7aded64
christian1251 <christianc905@csu.fullerton.edu>
2025-04-14 14:17:10 -0700

    set up some structuring

---
+++ shop/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+
+<h1>Brandname</h1>
+
+<nav>
+	<a class="link" href="/">Home</a>
+	<a class="link" href="/cart"> Cart</a>
+</nav>
+
+<style>
+	.link {
+		color: forestgreen;
+		text-decoration-line: underline;
+  		text-decoration-color: hotpink;
+	}
+</style>
+
+{@render children()}
--- shop/src/routes/+page.svelte
+++ shop/src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Listing from './listing.svelte';
+  </script>
+  
+  <h1>This is the homepage</h1>
+  
+  <form>Enter a product to search: </form>
+  <Listing />
