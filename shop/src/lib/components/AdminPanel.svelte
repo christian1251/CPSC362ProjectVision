f8b634ee1d6ddd131051d7b6df99748a9591c777
christian1251 <christianc905@csu.fullerton.edu>
2025-04-16 19:27:21 -0700

    Revert "finished setting up most of frontend"
    This reverts commit d43eacef4cef32e1db6832e80b065a199a58909a.

--- shop/src/lib/components/AdminPanel.svelte
+++ shop/src/lib/components/AdminPanel.svelte
@@ -1,15 +1,11 @@
 <script lang="ts" >
 
-    // Store of listings
     import { listings } from '$lib/stores/ListingsStore.js';
-
-    // Placeholders for adding or editting items
     let id = '', name = '', price = '', size = '', color = '';
     let editName = '', editPrice = '', editSize ='', editColor ='';
     let isEditing = false;
     let editId: number | null = null;
   
-    // Add a listing to the listings writable store
     function addListing() {
       listings.update(l => [...l, {
         id: Number(Date.now()),
@@ -18,14 +14,13 @@
       name = price = size = color = '';
     }
 
-    
-    // Remove a listing from the listings writable store
+   
     function removeListing(id: number) {
       listings.update(l => l.filter(item => item.id !== id));
     
     }
  
-    // Select an item to edit and pre-fill info in form
+    
     /**
 	 * @param {{ id: any; name: any; price: any; size: any; color: any; }} item
 	 */
@@ -38,7 +33,6 @@
         editColor = item.color;
     }
 
-    // After changing the selected item, update the information at that id 
     function confirmEdit() {
         listings.update(items => 
             items.map(item => item.id === editId
@@ -65,7 +59,6 @@
     <input placeholder="Color" bind:value={color}>
     <button on:click={addListing}>Add</button>
   {/if}
-
   <ul>
     {#each $listings as item}
       <li>
@@ -80,9 +73,5 @@
   <style>
     button {
       cursor: pointer;
-      border: 2px outset black ;
-      border-color: blue;
-      margin: 1rem;
-      background-color: cadetblue;
     }
   </style>
--- shop/src/lib/components/CartPanel.svelte
+++ shop/src/lib/components/CartPanel.svelte
@@ -1,28 +1,10 @@
 <script>
     import { cart } from "$lib/stores/Cartstores.js"
-	import { ssrImportMetaKey } from "vite/module-runner";
-
-    $: total = $cart.reduce((sum, item) => sum + item.price, 0);
-
 
 </script>
 
 <p>This page should add up the total of the listing prices, allow for removal of listing. have a checkout</p>
 
 {#each $cart as item }
-<p> This item is in the cart {item.id} - {item.name} - ${item.price}</p>
+<p> This item is in the cart {item.id} - {item.name}</p>
 {/each}
-
-
-<p>Total: ${total}</p>
-<button>Checkout</button>
-
-<style>
-     button {
-      cursor: pointer;
-      border: 2px outset black ;
-      border-color: blue;
-      margin: 1rem;
-      background-color: cadetblue;
-    }
-</style>
