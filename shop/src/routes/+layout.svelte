36db34ada37e4a843095950d4d17956a9532e231
Yuuji-95 <kobayashi.08120509@gmail.com>
2025-04-26 17:34:21 -0700

    removed top search bar

--- shop/src/routes/+layout.svelte
+++ shop/src/routes/+layout.svelte
@@ -6,19 +6,19 @@
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
-  
+
 	let menuOpen = false;
 	let dropdownEl: HTMLDivElement;
-  
+
 	function toggleMenu() {
 	  menuOpen = !menuOpen;
 	}
-  
+
 	function handleLogout() {
 	  signOut(auth);
 	  menuOpen = false;
 	}
-  
+
 	const onClickOutside = (e: MouseEvent) => {
 	  if (menuOpen && dropdownEl && !dropdownEl.contains(e.target as Node)) {
 		menuOpen = false;
@@ -28,15 +28,9 @@
 	  document.addEventListener('click', onClickOutside);
 	  return () => document.removeEventListener('click', onClickOutside);
 	});
-  
-	let searchTerm = '';
-	function handleSearch(e: Event) {
-	  e.preventDefault();
-	  goto(`/?search=${encodeURIComponent(searchTerm)}`);
-	}
-  </script>
-  
-  <header class="navbar">
+</script>
+
+<header class="navbar">
 	<div class="left-nav">
 	  <div class="brand">
 		<a href="/">StyleSpot</a>
@@ -46,20 +40,8 @@
 		<a href="/admin">Admin</a>
 	  </nav>
 	</div>
-  
+
 	<div class="icon-nav">
-	  <form class="search-form" on:submit={handleSearch}>
-		<input
-		  type="text"
-		  bind:value={searchTerm}
-		  placeholder="Search"
-		  aria-label="Search"
-		/>
-		<button type="submit">
-		  <i class="fa-solid fa-magnifying-glass"></i>
-		</button>
-	  </form>
-  
 	  <div class="dropdown" bind:this={dropdownEl}>
 		<button class="icon-link" on:click={toggleMenu} title="Account">
 		  <i class="fa-solid fa-user"></i>
@@ -75,14 +57,13 @@
 		  </ul>
 		{/if}
 	  </div>
-  
+
 	  <a class="icon-link" href="/cart" title="Cart">
 		<i class="fa-solid fa-shopping-cart"></i>
 	  </a>
 	</div>
-  </header>
-  
-  <main class="container">
+</header>
+
+<main class="container">
 	<slot />
-  </main>
-  
+</main>
